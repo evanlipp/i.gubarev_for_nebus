a05305f7e91557e7f81f7dee072eb21e94d42341
evanlipp <lippevan@gmail.com>
2026-09-03 14:23:23 +0300

    prepare test and style tooling`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["~/assets/styles/main.scss"],
   modules: ["@pinia/nuxt"],
   devServer: {
     port: 3001,
